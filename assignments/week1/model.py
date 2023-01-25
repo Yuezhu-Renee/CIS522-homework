25efe4c6fb3b47de7c4f4aa59bb9d1fdeec0ddc4
Yuezhu-Renee <78188014+Yuezhu-Renee@users.noreply.github.com>
2023-01-24 19:24:05 -0500

    hw1

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -25,7 +25,7 @@ class LinearRegression:
             Nothing.
         """
         X = np.c_[np.ones(X.shape[0]), X]
-        self.w = np.linalg.pinv(X.T @ X) @ (X.T @ y)
+        self.w = np.linalg.pinv(np.matmul(X.T,X) @ (X.T @ y))
         return None
 
     def predict(self, X: np.ndarray) -> np.ndarray:
