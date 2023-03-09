12f26fdbc4863ae9612bfba932b147b938394851
Yuezhu-Renee <78188014+Yuezhu-Renee@users.noreply.github.com>
2023-03-09 18:42:27 -0500

    hw4

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -17,14 +17,15 @@ class Model(torch.nn.Module):
         self.conv1 = nn.Conv2d(num_channels, 32, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(32, 32, 5)
-        self.fc1 = nn.Linear(32 * 5 * 5, 512)
-        self.fc2 = nn.Linear(512, 84)
-        self.fc3 = nn.Linear(84, num_classes)
+        # self.fc1 = nn.Linear(32 * 5 * 5, 512)
+        # self.fc2 = nn.Linear(512, 84)
+        # self.fc3 = nn.Linear(84, num_classes)
+        self.fc3 = nn.Linear(32 * 5 * 5, num_classes)
 
         nn.init.kaiming_normal_(self.conv1.weight)
         nn.init.kaiming_normal_(self.conv2.weight)
-        nn.init.kaiming_normal_(self.fc1.weight)
-        nn.init.kaiming_normal_(self.fc2.weight)
+        # nn.init.kaiming_normal_(self.fc1.weight)
+        # nn.init.kaiming_normal_(self.fc2.weight)
         nn.init.kaiming_normal_(self.fc3.weight)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -36,7 +37,7 @@ class Model(torch.nn.Module):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = x.view(-1, 32 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        # x = F.relu(self.fc1(x))
+        # x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
