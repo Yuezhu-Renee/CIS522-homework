55a9d3bc8a11c28599ba9d3dac544a20cf441d54
Yuezhu-Renee <78188014+Yuezhu-Renee@users.noreply.github.com>
2023-03-09 19:29:48 -0500

    hw4

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -14,11 +14,11 @@ class Model(torch.nn.Module):
         """
         super().__init__()
         self.conv_layer1 = nn.Conv2d(
-            in_channels=num_channels, out_channels=32, kernel_size=3
+            in_channels=num_channels, out_channels=16, kernel_size=3
         )
-        self.conv_layer2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
+        self.conv_layer2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3)
         self.max_pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc2 = nn.Linear(1152, num_classes)
+        self.fc2 = nn.Linear(576, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
